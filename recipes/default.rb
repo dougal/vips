dadbabeda1b199716054629bef7624d1dbdd24a0
Douglas F Shearer <dougal.s@gmail.com>
2014-11-27 12:57:33 +0000

    Make the vips version a variable.

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,21 +9,22 @@ unless ::File.exists?("/usr/local/lib/libvips.so")
   packages.each { |pkg| package pkg }
 
   vips_major_version = '7'
-  vips_version = '7.40.11'
+  vips_minor_version = '40'
+  vips_patch_version = '11'
+  vips_version = '#{vips_major_version}.#{vips_minor_version}.#{vips_patch_version}'
   
-
   # install vips from source
   bash "install vips" do
     cwd "/tmp"
     code <<-EOT
-    wget http://www.vips.ecs.soton.ac.uk/supported/7.40/vips-7.40.11.tar.gz 
-    tar zxf vips-7.40.11.tar.gz
-    cd vips-7.26.8
+    wget http://www.vips.ecs.soton.ac.uk/supported/#{vips_major_version}.#{vips_minor_version}/vips-#{vips_version}.tar.gz 
+    tar zxf vips-#{vips_version}.tar.gz
+    cd vips-#{vips_version}
     ./configure
     make
     make install
     ldconfig
-    rm -rf vips-7.40.11*
+    rm -rf vips-#{vips_version}*
     EOT
   end
 
